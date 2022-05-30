3cbfa7cbf3eb6d0fae9d2b40a902d7061ef432f6
admiller125 <95506767+admiller125@users.noreply.github.com>
2022-05-29 22:53:16 -0500

    Add files via upload

---
+++ Resources/PyPoll.py
@@ -0,0 +1,9 @@
+file_to_load = 'C:\Users\14074\Desktop\Resources'
+
+# Open the election results and read the file.
+election_data = open(file_to_load, 'r')
+
+# To do: perform analysis.
+
+# Close the file.
+election_data.close()
